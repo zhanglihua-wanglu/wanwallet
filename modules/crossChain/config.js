2f0dabecd60e98278b5632a65059454c19bde268
Zhang Lihua <zhang.lihua@wanglutech.com>
2018-05-06 10:36:43 +0800

    update log4js

--- modules/crossChain/config.js
+++ modules/crossChain/config.js
@@ -64,34 +64,34 @@ config.hasLocalNode = false;
 config.loglevel = 'debug';
 
 
-const logDebug = require('log4js');
-let log4jsOptions = {
-    appenders: {
-        ruleConsole: {type: 'console'},
-        ruleFile: {
-            type: 'dateFile',
-            filename: 'logs/server-',
-            pattern: 'yyyy-MM-dd.log',
-            maxLogSize: 10 * 1000 * 1000,
-            numBackups: 3,
-            alwaysIncludePattern: true
-        }
-    },
-    categories: {
-        default: {appenders: ['ruleConsole', 'ruleFile'], level: (config.loglevel || 'info')}
-    }
-};
-if(config.logfile)
-{
-    log4jsOptions.appenders.ruleFile = {
-        type: 'dateFile',
-        filename: config.logfile,
-        maxLogSize: 10 * 1000 * 1000,
-        alwaysIncludePattern: true
-    };
-    log4jsOptions.categories.default.appenders.push('ruleFile');
-}
-logDebug.configure(log4jsOptions);
+// const logDebug = require('log4js');
+// let log4jsOptions = {
+//     appenders: {
+//         ruleConsole: {type: 'console'},
+//         ruleFile: {
+//             type: 'dateFile',
+//             filename: 'logs/server-',
+//             pattern: 'yyyy-MM-dd.log',
+//             maxLogSize: 10 * 1000 * 1000,
+//             numBackups: 3,
+//             alwaysIncludePattern: true
+//         }
+//     },
+//     categories: {
+//         default: {appenders: ['ruleConsole', 'ruleFile'], level: (config.loglevel || 'info')}
+//     }
+// };
+// if(config.logfile)
+// {
+//     log4jsOptions.appenders.ruleFile = {
+//         type: 'dateFile',
+//         filename: config.logfile,
+//         maxLogSize: 10 * 1000 * 1000,
+//         alwaysIncludePattern: true
+//     };
+//     log4jsOptions.categories.default.appenders.push('ruleFile');
+// }
+// logDebug.configure(log4jsOptions);
 
 
 config.listOption = true;
