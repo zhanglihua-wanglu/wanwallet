f06b4e664cbe06e24d118fd1bdcbcb8d295873b3
MoLin <molin@wanchain.org>
2018-09-28 11:41:51 +0800

    update btc feeRate to use sdk default value.

--- modules/crossChainBtc/config.js
+++ modules/crossChainBtc/config.js
@@ -46,7 +46,6 @@ config.ethKeyStorePath = config.ethkeyStorePath;
 config.MAX_CONFIRM_BLKS = 100000000;
 config.MIN_CONFIRM_BLKS = 0;
 config.listOption = true;
-config.feeRate = 3000;
 config.gasLimit = 1000000;
 config.gasPrice = 200000000000;
 
