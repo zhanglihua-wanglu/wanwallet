7da5e175fc2fb683b8d515a5f8391d1f3cf275e0
Sean chai <chaiwl123@163.com>
2018-10-31 15:34:31 +0800

    update wanchain_js_sdk_testnet to wanchain_js_testnet

--- modules/crossChain/config_main.js
+++ modules/crossChain/config_main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const settings=require('../settings.js');
 
 config.network = "main";
-global.wanchain_js_sdk_testnet =  false;
+global.wanchain_js_testnet =  false;
 config.socketUrl = 'wss://api.wanchain.info';
 
 // log path
