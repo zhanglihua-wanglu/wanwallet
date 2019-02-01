09f53a77952426a0603a374d37857182366e5f69
Anson5555555Lee <frank5555555li@gmail.com>
2019-02-01 10:34:12 +0800

    add an interface to count pending cross chain transactions

--- modules/walletBackend/index.js
+++ modules/walletBackend/index.js
@@ -1,12 +1,29 @@
 require('./ccETH')
 require('./ccERC20')
 require('./ccBTC')
-const { walletCore } = require('wanchain-js-sdk')
+const { walletCore, ccUtil } = require('wanchain-js-sdk')
 const config = require('./config')
+const _ = require('lodash')
 
 // sdk singleton 
 const ccBackend = new walletCore(config)
 
-exports.init = async () => {
-    await ccBackend.init()
-}
+// pending cross chain transaction counter
+const pendingCrossChainTxCounter = () => { 
+    let crossCollectionEE = global.wanDb.getItemAll(config.crossCollection, {});
+    let crossCollectionBtc = ccUtil.getBtcWanTxHistory({})
+
+    _.remove(crossCollectionEE, (rec) => {
+        return rec.status === 'Redeemed' || rec.status === 'Revoked'
+    })
+
+    _.remove(crossCollectionBtc, (rec) => {
+        return rec.status === 'redeemFinished' || rec.status === 'revokeFinished' || rec.status === 'Success'
+    })
+
+    return crossCollectionEE.length + crossCollectionBtc.length
+} 
+
+exports.init = async () => { await ccBackend.init() }
+exports.ccPendingCounter = pendingCrossChainTxCounter
+
